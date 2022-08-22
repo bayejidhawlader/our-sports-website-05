e0b61c223cfcbda1a496bca0ec3123a32ebdb89a
bayejidhawlader <bayejidhawlader@gmail.com>
2022-08-23 00:23:59 +0600

    Changes Some Script Cose and find the right code

--- js/app.js
+++ js/app.js
@@ -1,123 +1,110 @@
-// inserts players into selection tab
 let count = 0;
-function insertIntoSelection(playerName) {
-  // checks to see if 5 players are already selected.
-  // if 5 selected, then it returns false, and it shows alert
+function insertSelection(playerNames) {
+
   console.log(document.getElementById('players-list').childElementCount);
   if (document.getElementById('players-list').childElementCount > 4) {
     alert('You Can\'t Add More Players');
     return false;
   }
-
   count++;
-
   const element = document.createElement('li');
-  element.innerHTML = count + '. ' + playerName;
+  element.innerHTML = count + '. ' + playerNames;
 
   document.getElementById('players-list').appendChild(element);
 }
 
-// disable button and change button color
-function disableButton(btnId) {
-  document.getElementById(btnId).disabled = true;
-  document.getElementById(btnId).style.backgroundColor = 'red';
+
+// button disable and change button color when clicked Button
+function disableBtn(playerBtnId) {
+  document.getElementById(playerBtnId).disabled = true;
+  document.getElementById(playerBtnId).style.backgroundColor = 'dark';
 }
 
-// retrieving players
-document
-.getElementById('player-one')
-.addEventListener('click', function (event) {
-  const flag = insertIntoSelection(event.target.value);
+
+// when click button then showing players name to Selected Field
+document.getElementById('player-one').addEventListener('click', function (event) {
+  const flag = insertSelection(event.target.value);
 
   if (flag === false) {
     return;
   }
-  disableButton('player-one');
+  disableBtn('player-one');
 });
-document
-.getElementById('player-two')
-.addEventListener('click', function (event) {
-  const flag = insertIntoSelection(event.target.value);
+
+document.getElementById('player-two').addEventListener('click', function (event) {
+  const flag = insertSelection(event.target.value);
 
   if (flag === false) {
     return;
   }
-  disableButton('player-two');
+  disableBtn('player-two');
 });
-document
-.getElementById('player-three')
-.addEventListener('click', function (event) {
-  const flag = insertIntoSelection(event.target.value);
+
+document.getElementById('player-three').addEventListener('click', function (event) {
+  const flag = insertSelection(event.target.value);
 
   if (flag === false) {
     return;
   }
-  disableButton('player-three');
+  disableBtn('player-three');
 });
-document
-.getElementById('player-four')
-.addEventListener('click', function (event) {
-  const flag = insertIntoSelection(event.target.value);
+
+document.getElementById('player-four').addEventListener('click', function (event) {
+  const flag = insertSelection(event.target.value);
 
   if (flag === false) {
     return;
   }
-  disableButton('player-four');
+  disableBtn('player-four');
 });
-document
-.getElementById('player-five')
-.addEventListener('click', function (event) {
-  const flag = insertIntoSelection(event.target.value);
+
+document.getElementById('player-five').addEventListener('click', function (event) {
+  const flag = insertSelection(event.target.value);
 
   if (flag === false) {
     return;
   }
-  disableButton('player-five');
+  disableBtn('player-five');
 });
-document
-.getElementById('player-six')
-.addEventListener('click', function (event) {
-  const flag = insertIntoSelection(event.target.value);
+
+document.getElementById('player-six').addEventListener('click', function (event) {
+  const flag = insertSelection(event.target.value);
 
   if (flag === false) {
     return;
   }
-  disableButton('player-six-btn');
+  disableBtn('player-six-btn');
 });
 
-// inserts data into text field
-function insertIntoTextField(inputId, data) {
+
+
+// Get data into the text field
+function insertDataIntoTextField(inputId, data) {
   document.getElementById(inputId).innerText = data;
 }
 
-// retrieves data from input field
+// Receiving data from the input field
 function getDataFromInputField(inputId) {
   return parseInt(document.getElementById(inputId).value);
 }
 
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
-  // retrieves data for each player
+  // Receiving data for each player
   const costPerPlayer = getDataFromInputField('per-player-field');
 
-  // enables dollar symbol
-  document.getElementById('player-expense').style.display = 'inline';
-
   // inserts the total cost for players
-  insertIntoTextField('player-span-expense', costPerPlayer * count);
+  insertDataIntoTextField('player-span-expense', costPerPlayer * count);
 });
 
-document
-  .getElementById('calculate-total')
-  .addEventListener('click', function () {
+
+document.getElementById('calculate-total').addEventListener('click', function () {
     const costPerPlayer = parseInt(
-      document.getElementById('player-span-expense').innerText
-    );
-    const managerCost = getDataFromInputField('manager-costs');
-    const coachCost = getDataFromInputField('coach-costs');
+    document.getElementById('player-span-expense').innerText);
+    const managerCosts = getDataFromInputField('manager-costs');
+    const coachCosts = getDataFromInputField('coach-costs');
 
-    const totalCost = costPerPlayer + managerCost + coachCost;
+    const totalCosts = costPerPlayer + managerCosts + coachCosts;
 
-    document.getElementById('total-expense').style.display = 'inline';
-
-    insertIntoTextField('total-span-expense', totalCost);
+    insertDataIntoTextField('total-span-expense', totalCosts);
   });
